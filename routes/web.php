29c0102f2b8d2f3e7ed8276054789d176e181d1d
Mathilde1420 <kassemathilde@gmail.com>
2023-11-06 19:32:31 +0000

    Fusion

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/{vehicle}/map', [Controller::class, 'maps']);
+Route::get('/driver/{driver}/vehicle/{vehicle}/{date}/map', [Controller::class, 'maps']);
+
+
+// ---------DRIVER------------
+Route::get('/dashboard', 'App\Http\Controllers\DriverController@index')->name("driver.index");
+Route::get('/profil', 'App\Http\Controllers\DriverController@profil')->name("driver.profil");
+Route::get('/drivers', 'App\Http\Controllers\DriverController@driverlist')->name("driver.listing");
+Route::get('/signout', 'App\Http\Controllers\DriverController@signout')->name("driver.signout");
+Route::get('/', 'App\Http\Controllers\DriverController@sign')->name("driver.sign");
+
+// ---------VEHICULE----------
+Route::get('/track', 'App\Http\Controllers\VehicleController@track')->name("vehicle.track");
+Route::get('/fuel', 'App\Http\Controllers\VehicleController@fuel')->name("vehicle.fuel");
+
+Route::get('/maintenance', 'App\Http\Controllers\VehicleController@maintenance')->name("vehicle.maintenance");
+
+
 
